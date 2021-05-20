3fd43e27d0db661cf2134f80b8bebd517b25600c
decagon <anietieudoaka@gmail.com>
2021-05-20 01:38:27 +0100

    Main Activity

--- app/src/main/java/com/decagon/android/sq007/MainActivity.kt
+++ app/src/main/java/com/decagon/android/sq007/MainActivity.kt
@@ -1,14 +1,23 @@
 package com.decagon.android.sq007
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var openMap: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        openMap = findViewById(R.id.buttonOpenMap)
+        openMap.setOnClickListener {
+            val mapIntent = Intent(this, TrackerActivity::class.java)
+            startActivity(mapIntent)
+        }
     }
 
-//    var userLocation = MyLocation(location.latitude!!, location.longitude!!)
-//  dbReference.child("Anietie").setValue(userLocation)
 }
